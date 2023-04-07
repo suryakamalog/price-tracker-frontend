35368da9dc16d9e4c8e21b2c85f0aeef8d926445
skamal <skamal@qti.qualcomm.com>
2023-04-07 22:45:12 +0530

    added deletion of product

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -18,14 +18,36 @@ const ProductList = () => {
       } catch (err) {
         console.log(err);
       }
-    }
-      getAllProducts();
-      return () => {
-        isMounted = false;
-        controller.abort();
-      };
-    
-  });
+    };
+    getAllProducts();
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
+  const deleteProductHandler = (id) => {
+    const deleteProduct = async (id) => {
+      try {
+        console.log(id);
+        const response = await axiosPrivate.delete(
+          "/api/user_product/delete/",
+          {
+            data: {
+              "id": id,
+            },
+
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setProducts(products.filter((product) => product.pk !== id));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    deleteProduct(id);
+  };
   return (
     <>
       <div className="container-sm">
@@ -41,8 +63,21 @@ const ProductList = () => {
             {products.map((product, key) => (
               <tr key={key}>
                 <td>{product.fields.website}</td>
-                <td>{product.fields.url}</td>
+                <td>
+                  <a href={product.fields.url}>{product.fields.url}</a>
+                </td>
                 <td>{product.fields.price}</td>
+                <td>
+                  <button className="btn btn-secondary">Edit</button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteProductHandler(product.pk)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
